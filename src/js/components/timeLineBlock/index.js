4cfc6bbc40f088315d30a753eb594e2a9503fe1a
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-06-13 00:00:11 +0300

    test

--- src/js/components/timeLineBlock/index.js
+++ src/js/components/timeLineBlock/index.js
@@ -4,20 +4,20 @@ import {Link, Redirect} from 'react-router-dom';
 class TimeLineBlock extends React.Component {
 
     toggleCheck() {
-        document.querySelector('.header-time-line-section__check div').classList.toggle('checked');
+        document.querySelector('.time-line-section__check div').classList.toggle('checked');
     }
 
     render() {
         return (
             <div>
-                    <div className="container">
-                        <div className="header-time-line-section-wrapper">
-                            <p>Time line</p>
-                            <div className="header-time-line-section__check" onClick={this.toggleCheck}>
-                                <div className=""></div>
-                            </div>
+                <div className="container">
+                    <div className="time-line-section-wrapper">
+                        <p>Time line</p>
+                        <div className="time-line-section__check" onClick={this.toggleCheck}>
+                            <div className=""></div>
                         </div>
                     </div>
+                </div>
             </div>
         )
     }
