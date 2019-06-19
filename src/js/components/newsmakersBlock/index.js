2c4f916ab15fb39f1285d3506c4c30cdf3d74154
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-06-19 18:12:54 +0300

    hp

--- src/js/components/newsmakersBlock/index.js
+++ src/js/components/newsmakersBlock/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Scrollbar from "react-scrollbars-custom";
 import {Link} from "react-router-dom";
 import { Scrollbars } from 'react-custom-scrollbars';
 
@@ -61,7 +60,6 @@ class NewsmakersBlock extends React.Component {
                         </div>
                     </div>
                     <div className="scroll-newsmakers-block">
-                        {/*<Scrollbar className="list-news">*/}
                             <Scrollbars className="list-news" hideTracksWhenNotNeeded={true} autoHeight={this.state.is_mobile}>
                             <div className="scroll-select-news">
                                 <div className="scroll-select-news__img">
@@ -170,34 +168,6 @@ class NewsmakersBlock extends React.Component {
                                 </div>
                             </div>
                             <div className="scroll-select-news">
-                                <div className="scroll-select-news__preview">
-                                    <div className="scroll-select-news__preview__title">
-                                        <Link to='/'>Mauricio Pochettino praised Tottenham's</Link>
-                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
-                                    </div>
-                                    <div className="scroll-select-news__preview__info">
-                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time_icon.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="scroll-select-news">
-                                <div className="scroll-select-news__img">
-                                    <img src="img/JMP_Bristol_Rugby_v_Bedford_Blues_RT0307.jpg" alt=""/>
-                                </div>
-                                <div className="scroll-select-news__preview">
-                                    <div className="scroll-select-news__preview__title">
-                                        <Link to='/'>Mauricio Pochettino praised Tottenham's</Link>
-                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
-                                    </div>
-                                    <div className="scroll-select-news__preview__info">
-                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time_icon.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="scroll-select-news">
-                                <div className="scroll-select-news__img">
-                                    <img src="img/Minis-landscape2.jpg" alt=""/>
-                                </div>
                                 <div className="scroll-select-news__preview">
                                     <div className="scroll-select-news__preview__title">
                                         <Link to='/'>Mauricio Pochettino praised Tottenham's</Link>
--- src/js/components/videoBlock/index.js
+++ src/js/components/videoBlock/index.js
@@ -1,8 +1,26 @@
 import React from "react";
-import Scrollbar from "react-scrollbars-custom";
 import {Link} from "react-router-dom";
+import {Scrollbars} from "react-custom-scrollbars";
 
 class VideoBlock extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            is_mobile: false
+        };
+
+        this.isMobileCheck = this.isMobileCheck.bind(this);
+    }
+
+    isMobileCheck() {
+        return (/iPhone|iPad|iPod|Android|webOS/.test(navigator.userAgent) && !window.MSStream);
+    }
+
+    componentDidMount() {
+        this.setState({is_mobile: this.isMobileCheck()})
+    }
 
     render() {
         return (
@@ -35,7 +53,7 @@ class VideoBlock extends React.Component {
                         </div>
                     </div>
                     <div className="scroll-newsmakers-block">
-                        <Scrollbar className="list-news">
+                        <Scrollbars className="list-news" hideTracksWhenNotNeeded={true} autoHeight={this.state.is_mobile}>
                             <div className="scroll-select-news">
                                 <div className="scroll-select-news__img">
                                     <img src="img/JMP_Bristol_Rugby_v_Bedford_Blues_RT0307.jpg" alt=""/>
@@ -153,35 +171,7 @@ class VideoBlock extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="scroll-select-news">
-                                <div className="scroll-select-news__img">
-                                    <img src="img/JMP_Bristol_Rugby_v_Bedford_Blues_RT0307.jpg" alt=""/>
-                                </div>
-                                <div className="scroll-select-news__preview">
-                                    <div className="scroll-select-news__preview__title">
-                                        <Link to='/'>Mauricio Pochettino praised Tottenham's</Link>
-                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
-                                    </div>
-                                    <div className="scroll-select-news__preview__info">
-                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time_icon.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="scroll-select-news">
-                                <div className="scroll-select-news__img">
-                                    <img src="img/Minis-landscape2.jpg" alt=""/>
-                                </div>
-                                <div className="scroll-select-news__preview">
-                                    <div className="scroll-select-news__preview__title">
-                                        <Link to='/'>Mauricio Pochettino praised Tottenham's</Link>
-                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
-                                    </div>
-                                    <div className="scroll-select-news__preview__info">
-                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time_icon.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Scrollbar>
+                        </Scrollbars>
                     </div>
                     <div className="show-more-news">
                         <button>More Recent News <span>&rarr;</span></button>
