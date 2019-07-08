5b134e8bdf3690c602221767362002d9e51832c0
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-07-08 17:48:21 +0300

    style

--- src/js/components/resultFixturesBlock/index.js
+++ src/js/components/resultFixturesBlock/index.js
@@ -3,19 +3,23 @@ import {Link} from "react-router-dom";
 
 class ResultFixturesBlock extends React.Component {
 
+    toggleResultFixtures() {
+
+    }
+
     render() {
         return (
             <div>
                 <div className="result-fixtures-block">
-                    <div className="result-fixtures-block__title">
-                        <div className="result-fixtures-block__title-result">
+                    <div className="result-fixtures-block__title" onClick={this.toggleResultFixtures}>
+                        <div className="result-fixtures-block__title-result active">
                             <p>RESULT</p>
                         </div>
-                        <div className="result-fixtures-block__title-fixtures">
+                        <div className="result-fixtures-block__title-fixtures active">
                             <p>fixtures</p>
                         </div>
                     </div>
-                    <div className="result-fixtures-block__table">
+                    <div className="result-fixtures-block__table__result">
                         <div className="result-fixtures-block__table-result">
                             <div className="result-fixtures-block__table-result-info">
                                 <p>
@@ -185,6 +189,10 @@ class ResultFixturesBlock extends React.Component {
                             </div>
                         </div>
                         <Link to='/' className="result-fixtures-block__table-link">All Result <span> &rarr;</span></Link>
+                    </div>
+
+                    <div className="result-fixtures-block__table__fixtures">
+
                     </div>
                 </div>
             </div>
